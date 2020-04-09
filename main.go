f3b0ba79d23148954ca72ebbcb483e74e1801f35
Ishuah Kariuki <kariuki@ishuah.com>
2020-04-09 20:15:05 +0300

    Proper logging

--- main.go
+++ main.go
@@ -35,12 +35,24 @@ func main() {
 		}
 
 		packet := Packet(frame.Data())
-		if packet.Protocol() != 17 {
-			continue
-		}
 
-		udpDatagram := UDPDatagram(packet.Options())
-		log.Printf("%v", packet.IHL())
-		log.Printf("received message: %s", udpDatagram.Body())
+		log.Println("Frame Data:")
+		log.Printf("\tSource MAC Address: %v\n", frame.Source())
+		log.Printf("\tDestination MAC Address: %v\n", frame.Destination())
+		log.Println("Packet Data:")
+		log.Printf("\tVersion: %v\n", packet.Version())
+		log.Printf("\tIHL: %v\n", packet.IHL())
+		log.Printf("\tProtocol: %v\n", packet.Protocol())
+		log.Printf("\tSource IP: %v\n", packet.SourceIP())
+		log.Printf("\tDestination IP: %v\n", packet.DestinationIP())
+		if packet.Protocol() == 17 {
+			udpDatagram := UDPDatagram(packet.Options())
+			log.Println("UDP Datagram:")
+			log.Printf("\tSource port: %v", udpDatagram.SourcePort())
+			log.Printf("\tDestination port %v", udpDatagram.DestinationPort())
+			log.Printf("\tLength: %v", udpDatagram.Length())
+			log.Printf("\tBody: %v", udpDatagram.Body())
+		}
+		log.Println("----------------------------------------------------")
 	}
 }
